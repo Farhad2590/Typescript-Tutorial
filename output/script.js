ab46218a12f87e9810cf3382f81cdbbf99b09e12
myusername <farhadhossen2590@gmail.com>
2024-09-19 12:39:15 +0600

    first commit

---
+++ output/script.js
@@ -0,0 +1,3 @@
+console.log('Hello');
+var Country = 'Bangladesh';
+console.log(Country);
---
+++ src/script.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log('Hello');
+const Country = 'I Love Bangladesh more';
+console.log(Country);
+let playerName = "Mashrafee";
+let age = 56;
+// playerName = 34 [Wrong]
+age = 46;
+// Typescript solve the problem in code editor this is the special feature
+let player;
+console.log(player); //undefined
+player = "Farhad";
+player = 22; //undefined is not data type so we can re assign this eassily
