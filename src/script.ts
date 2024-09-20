23a97cf1cc959c6db5447388882b2eb2cfc5f259
myusername <farhadhossen2590@gmail.com>
2024-09-21 00:15:55 +0600

    About Class

--- src/script.ts
+++ src/script.ts
@@ -199,3 +199,26 @@ calculation = ( a: number , b : number , c : string) =>{
 }
 
 console.log(calculation(10,5, 'Add'));
+
+
+class Player {
+    name : string;
+    age : number;
+    country : string;
+
+    constructor(n:string, a:number, c:string){
+        this.name = n;
+        this.age = a;
+        this.country = c
+    }
+    play(){
+        console.log(`${this.name} from ${this.age} is palying`);
+        
+    }
+}
+const mashrafi = new Player('Mashrafi',40,'Bangladesh')
+const sakib = new Player('Sakib',38,'Bangladesh')
+
+const players : Player[] = []
+
+players.push(mashrafi)
